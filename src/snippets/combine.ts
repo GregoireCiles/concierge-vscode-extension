c530896653b270d36f480976da03883334de7371
Gregoire Ciles <gregoire.ciles@protonmail.com>
2022-03-22 22:50:30 +0100

    fix(snippets): change output directory

--- src/snippets/combine.ts
+++ src/snippets/combine.ts
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const snippetFolders = ['javascript']
+const snippetsOutDir = path.join(__dirname, '../../out')
 const jsonCommentRegex = new RegExp(/(\n\s+\/\/.*)/, 'gi')
 const snippetsParentDir = path.join(__dirname, '../../src/snippets')
 
@@ -13,7 +14,7 @@ for (const snippetFolder of snippetFolders) {
     const json = fs.readFileSync(filePath).toString('utf8').replace(jsonCommentRegex, '').trim()
 
     Object.assign(snippets, JSON.parse(json))
-    const output = path.join(snippetsParentDir, 'output', `${snippetFolder}.json`)
+    const output = path.join(snippetsOutDir, 'snippets', `${snippetFolder}.json`)
     fs.writeFileSync(output, JSON.stringify(snippets))
   }
 }
